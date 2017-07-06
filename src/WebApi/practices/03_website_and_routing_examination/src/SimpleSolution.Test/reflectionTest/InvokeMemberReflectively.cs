4b05e7c982da5f4ad34987737c0123bec9fe9f0f
mjiao <mjiao@thoughtworks.com>
2017-07-06 23:45:02 +0800

    [jiaoming] practice reflection invoke method and property.

---
+++ src/WebApi/practices/03_website_and_routing_examination/src/SimpleSolution.Test/reflectionTest/InvokeMemberReflectively.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Xunit;
+
+namespace CSharpViaTest.OtherBCLs.HandleReflections
+{
+    /* 
+     * Description
+     * ===========
+     * 
+     * This test will try invoke instance's member via reflection.
+     * 
+     * Difficulty: Medium
+     * 
+     * Knowledge Point
+     * ===============
+     * 
+     * - MethodInfo.Invoke()
+     * - PropertyInfo.GetValue() / SetValue()
+     * 
+     * Requirement
+     * ===========
+     * 
+     * - All the operations should be accomplished by reflection.
+     */
+    public class InvokeMemberReflectively
+    {
+        [SuppressMessage("ReSharper", "UnusedMember.Local", Justification = "It will be called via reflection.")]
+        class ReflectionSample
+        {
+            public string Say(string name)
+            {
+                return $"Hello {name}!";
+            }
+
+            public string Id { get; set; }
+            public string Readonly { get; private set; }
+            public string AnotherReadonly => "Hello";
+            public string ThrowsProperty { get { throw new NotSupportedException(); } }
+        }
+
+        #region Please modifies the code to pass the test
+        
+        static object InvokeMethod(object instance, string methodName, params object[] args)
+        {
+            if (instance == null) throw new ArgumentNullException(nameof(instance));
+            if (methodName == null) throw new ArgumentNullException(nameof(methodName));
+
+            MethodInfo methodInfo = instance.GetType().GetMethod(methodName);
+
+            if (methodInfo == null) throw new InvalidOperationException();
+            return methodInfo.Invoke(instance, args);
+        }
+
+        static void SetProperty(object instance, string propertyName, object value)
+        {
+            if (instance == null) throw new ArgumentNullException(nameof(instance));
+            if (propertyName == null) throw new ArgumentNullException(nameof(propertyName));
+
+            PropertyInfo propertyInfo = instance.GetType().GetProperty(propertyName);
+
+            if (propertyInfo != null && propertyInfo.CanWrite && propertyInfo.SetMethod.IsPublic)
+            {
+                propertyInfo.SetValue(instance, value);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        static object GetProperty(object instance, string propertyName)
+        {
+            if (instance == null) throw new ArgumentNullException(nameof(instance));
+            if (propertyName == null) throw new ArgumentNullException(nameof(propertyName));
+
+            PropertyInfo propertyInfo = instance.GetType().GetProperty(propertyName);
+            
+            object propertyValue = null;
+            try
+            {
+                propertyValue = propertyInfo.GetValue(instance);
+            }
+            catch (TargetInvocationException targetInvocationException)
+            {
+                throw targetInvocationException.InnerException;
+            }
+            return propertyValue;
+        }
+
+        #endregion
+
+        [Fact]
+        public void should_throw_if_instance_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(
+                "instance", 
+                () => InvokeMethod(null, "MethodName"));
+        }
+
+        [Fact]
+        public void should_throw_if_methodName_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(
+                "methodName",
+                () => InvokeMethod(new ReflectionSample(), null));
+        }
+
+        [Fact]
+        public void should_throw_if_method_not_found()
+        {
+            Assert.Throws<InvalidOperationException>(
+                () => InvokeMethod(new ReflectionSample(), "NotExistedMethod"));
+        }
+
+        [Fact]
+        public void should_invoke_method()
+        {
+            object result = InvokeMethod(new ReflectionSample(), "Say", "the name");
+
+            Assert.Equal("Hello the name!", result);
+        }
+
+        [Fact]
+        public void should_throw_if_instance_is_null_when_set_property()
+        {
+            Assert.Throws<ArgumentNullException>(
+                "instance",
+                () => SetProperty(null, "Id", "value"));
+        }
+
+        [Fact]
+        public void should_throw_if_propertyName_is_null_when_set_property()
+        {
+            Assert.Throws<ArgumentNullException>(
+                "propertyName",
+                () => SetProperty(new ReflectionSample(), null, "value"));
+        }
+
+        [Fact]
+        public void should_throw_if_propertyName_not_exist()
+        {
+            Assert.Throws<InvalidOperationException>(
+                () => SetProperty(new ReflectionSample(), "NotExistedProp", "value"));
+        }
+
+        [Theory]
+        [InlineData("Readonly")]
+        [InlineData("AnotherReadonly")]
+        public void should_throw_if_property_has_no_public_setter(string readonlyPropName)
+        {
+            Assert.Throws<InvalidOperationException>(
+                () => SetProperty(new ReflectionSample(), readonlyPropName, "value"));
+        }
+
+        [Fact]
+        public void should_set_and_get_property()
+        {
+            var instance = new ReflectionSample();
+            const string expectedId = "SuperCoolId";
+
+            SetProperty(instance, "Id", expectedId);
+            Assert.Equal(expectedId, GetProperty(instance, "Id"));
+        }
+
+        [Fact]
+        public void should_get_original_exception()
+        {
+            var instance = new ReflectionSample();
+            Assert.Throws<NotSupportedException>(() => GetProperty(instance, "ThrowsProperty"));
+        }
+    }
+}
