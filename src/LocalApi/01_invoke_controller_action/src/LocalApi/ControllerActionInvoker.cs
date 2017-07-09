3f0b7b7792127f4226dcdb4d69d98c0e7e4792f1
mjiao <mjiao@thoughtworks.com>
2017-07-09 20:11:21 +0800

    [jiaoming] practice invoke method in local.

--- src/LocalApi/01_invoke_controller_action/src/LocalApi/ControllerActionInvoker.cs
+++ src/LocalApi/01_invoke_controller_action/src/LocalApi/ControllerActionInvoker.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Reflection;
 
 namespace LocalApi
 {
@@ -7,7 +10,27 @@ namespace LocalApi
     {
         public static HttpResponseMessage InvokeAction(ActionDescriptor actionDescriptor)
         {
-            throw new NotImplementedException();
+            HttpController httpController = actionDescriptor.Controller;
+            var actionName = actionDescriptor.ActionName;
+
+            Type type = httpController.GetType();
+            MethodInfo methodInfo = type
+                .GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                .SingleOrDefault(method => method.Name.Equals(actionName, StringComparison.OrdinalIgnoreCase));
+
+            if (methodInfo == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
+            try
+            {
+                return (HttpResponseMessage) methodInfo.Invoke(httpController, null);
+            }
+            catch
+            {
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
         }
     }
 }
