50ed8ea2666779860de6872e5c4f11b3f8e68c10
mjiao <mjiao@thoughtworks.com>
2017-07-12 00:38:06 +0800

    [jiaoming] practice to get controller by controller type.

--- src/LocalApi/03_invoke_controller_action_from_route/src/LocalApi/DefaultHttpControllerTypeResolver.cs
+++ src/LocalApi/03_invoke_controller_action_from_route/src/LocalApi/DefaultHttpControllerTypeResolver.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace LocalApi
@@ -15,7 +16,9 @@ namespace LocalApi
         
         public ICollection<Type> GetControllerTypes(IEnumerable<Assembly> assemblies)
         {
-            throw new NotImplementedException();
+            return assemblies.SelectMany(a => a.GetTypes())
+                .Where(a => a.IsSubclassOf(typeof(HttpController)) && a.IsPublic && !a.IsAbstract)
+                .ToList();
         }
 
         #endregion
